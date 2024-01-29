08bfa5d2c9e753aac00bae8b55598157c1a8306c
Mr-Keys <31483590+Mr-Keys@users.noreply.github.com>
2024-01-29 20:58:30 +0530

    added optional parameters and default parameters to get method

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
-#basic endpoint to get root
+
+# basic endpoint to get root
 @app.get('/')
 def index():
     return {'message': 'Hello World'}
 
 
-@app.get('/blog/all')
-def get_all_blogs():
-    return "all blogs"
+# Adding query parameters.(Things not in path=== query parameters)
+# Also adding default values by using '='
+# Adding optional parameters byu using Optional [datatype]
 
-#Add a enum Class to restrict user inputs
+# @app.get('/blog/all')
+# def get_all_blogs(page=1,page_size=10):
+#  return f'all {page_size} pages on page {page}'
+
+@app.get('/blog/all')
+def get_all_blogs(page=1, page_size: Optional[int] = None):
+    return {'message': f'page{page} page size={page_size}'}
+
+
+# Add a enum Class to restrict user inputs
 class BlogType(str, Enum):
     short = 'short'
     story = 'story'
