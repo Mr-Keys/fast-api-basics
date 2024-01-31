28ce8e0fb7a67798abe80061c375078743cb2961
Mr-Keys <31483590+Mr-Keys@users.noreply.github.com>
2024-01-31 20:34:06 +0530

    added tags summary description and response description

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 from typing import Optional
 
@@ -19,13 +19,13 @@ def index():
 # def get_all_blogs(page=1,page_size=10):
 #  return f'all {page_size} pages on page {page}'
 
-@app.get('/blog/all')
+@app.get('/blog/all',tags=['blog'])
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {'message': f'page{page} page size={page_size}'}
 
 
 # combining path parameters with query parameters
-@app.get('/blog/{id}/comments/{comment_id)')
+@app.get('/blog/{id}/comments/{comment_id)',tags=['comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
     return {'message': f'blog_id{id},comment_id {comment_id}'}
 
@@ -37,11 +37,38 @@ class BlogType(str, Enum):
     howto = 'howto'
 
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}',tags=['blog'])
 def get_blog_from_type(type: BlogType):
     return {'message': f'BlogType: {BlogType[type].value}'}
 
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'blog': f"Blog no {id}"}
+# adding status code
+@app.get(
+    '/blog/{id}', status_code=status.HTTP_404_NOT_FOUND,
+    tags=['blog'],
+    summary='Get a particular blog based on the blog id provided',
+    description='From multiple blogs in a database get a particular blog based on the blog id',
+    response_description='List of available vlogs'
+)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'blog': f"Blog no {id}"}
+
+#below is a more optimised way for the above method.
+@app.get('/blogz/{id}',tags=['blog'])
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
+    else:
+        return {'blog': f"Blog no {id}"}
+
+
+
+#tags-used to define categories for different API calls
+#summary-short description of what the API does and description-details about the API(long)
+#can also use ''' some description here ''' inside a function declaration for description
