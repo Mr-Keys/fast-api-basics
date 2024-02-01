74c29051aaae87cddf373215a8435de9a427e4d9
Mr-Keys <31483590+Mr-Keys@users.noreply.github.com>
2024-02-01 20:10:56 +0530

    added some code

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 router = APIRouter(
     prefix='/blog',
@@ -24,3 +24,17 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'version': version
 
     }
+
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel, id: int,
+                   comment_id: int = Query(None,
+                                           title='comment id of the post',
+                                           description='comment id is the id associated with each post',
+                                           deprecated=True
+
+                                           ), content: str = Body('Hii how are you', min_length=10, regex='^[a-z\s]*$'),
+                   some_other_key: str = Body(...),
+                   v: Optional[List[str]]= Query(None)
+                   ):
+    return {'data': f'id {id}'}
