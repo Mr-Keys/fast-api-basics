9faff43462d1c2ae56cf615bed6afae434f4b5d3
nirupam <nirupamsharma68@gmail.com>
2024-02-01 16:28:23 +0530

    added parameters

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter(
     prefix='/blog',
@@ -6,6 +8,19 @@ router = APIRouter(
 )
 
 
-@router.post('/')
-def create_blog():
-    return {'message': 'hello world'}
+class BlogModel(BaseModel):
+    title: int
+    content: str
+    no_of_comments: int
+    published: Optional[bool] = False
+
+
+# combining parameters
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'id': id,
+        'data': blog,
+        'version': version
+
+    }
