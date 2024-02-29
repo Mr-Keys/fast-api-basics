67ccaf536b026c2b513bf67bc4ffcafacbfb9414
Mr-Keys <31483590+Mr-Keys@users.noreply.github.com>
2024-02-29 08:33:27 +0530

    added articles router

--- schemas.py
+++ schemas.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
@@ -7,9 +9,47 @@ class UserBase(BaseModel):
     password: str
 
 
+# Article inside UserDisplay
+class Article(BaseModel):
+    title: str
+    content: str
+    published: bool
+
+    class Config():
+        from_attributes = True
+
+
 class UserDisplay(BaseModel):
     username: str
     email: str
+    items: List[Article] = []
 
     class Config():
-        orm_mode = True
+        from_attributes = True
+
+
+class ArticleBase(BaseModel):
+    title: str
+    content: str
+    published: bool
+    creator_id: int
+
+
+# User inside article display
+
+class User(BaseModel):
+    id: int
+    username: str
+
+    class Config():
+        from_attributes = True
+
+
+class ArticleDisplay(BaseModel):
+    title: str
+    content: str
+    published: bool
+    user: User
+
+    class Config():
+        from_attributes = True
